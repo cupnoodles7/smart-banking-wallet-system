4dce7842e9ca65ff850a647b805f51ca2dee3c0a
avs-3214 <avvarusvaishnavi@gmail.com>
2026-05-18 17:51:35 +0530

    Cleaned Main.java

--- main/Main.java
+++ main/Main.java
@@ -1,38 +1,9 @@
 package main;
 
-import wallet.*;
-import exception.*;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        PaytmWallet paytm = new PaytmWallet(10000);
-        PhonePeWallet phonepe = new PhonePeWallet(5000);
-
-        try {
-
-            paytm.addMoney(2000);
-
-            paytm.payBill(3000);
-
-            paytm.transferToWallet(4000, phonepe);
-
-            paytm.displayBalance();
-            phonepe.displayBalance();
-
-        }
-        catch (WalletLimitExceededException e) {
-            System.out.println(e.getMessage());
-        }
-        catch (InsufficientBalanceException e) {
-            System.out.println(e.getMessage());
-        }
-        catch (InvalidAmountException e) {
-            System.out.println(e.getMessage());
-        }
-        finally {
-            System.out.println("Wallet operation completed");
-        }
+        System.out.println("Smart Banking System Started");
     }
 }
